77734e281ffb86d08d430dbe8013a7e32e5846a0
estaruma <esther.arumainayagam@mettle.co.uk>
2024-09-02 14:24:44 +0100

    Toppings class

--- src/main/java/org/example/Toppings.java
+++ src/main/java/org/example/Toppings.java
@@ -1,5 +1,24 @@
 package org.example;
 
 // allow toppings
-public class Toppings {
+public class Toppings extends Cake {
+    private Cake baseCake;
+
+    public Toppings (Cake baseCake, String topping, double toppingPrice){
+        super(baseCake.getName() + " with " + topping, baseCake.getPrice() + toppingPrice);
+        this.baseCake = baseCake;
+    }
+
+    @Override
+    public String getName() {
+        return baseCake.getName();
+    }
+
+    @Override
+    public double getPrice() {
+        return baseCake.getPrice();
+    }
+
+
+
 }
